1adc0f3166da69d8678a39f0e82171154ebb6780
Natasa Mori <natasa@the-mori.com>
2018-07-27 10:57:51 +0200

    Starting point of the app - based on Udacity's ud845-Pets project

---
+++ app/src/main/java/com/example/natasa/inventoryapp/EditorActivity.java
@@ -0,0 +1,72 @@
+package com.example.natasa.inventoryapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+/**
+ * Allows user to create a new book entry or edit an existing one.
+ */
+public class EditorActivity extends AppCompatActivity {
+
+    /** EditText field to enter the book's name */
+    private EditText nameEditText;
+
+    /** EditText field (inputType="numberDecimal") to enter price of the product */
+    private EditText priceEditText;
+
+    /** EditText field to enter quantity */
+    private EditText quantityEditText;
+
+    /** EditText field to enter supplier name */
+    private EditText supplierNameEditText;
+
+    /** EditText field to enter supplier phone number */
+    private EditText supplierPhoneNumberEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_editor);
+
+        // Find all relevant views that we will need to read user input from
+        nameEditText = (EditText) findViewById(R.id.edit_product_name);
+        priceEditText = (EditText) findViewById(R.id.edit_product_price);
+        quantityEditText = (EditText) findViewById(R.id.edit_quantity);
+        supplierNameEditText = (EditText) findViewById(R.id.edit_supplier_name);
+        supplierPhoneNumberEditText = (EditText) findViewById(R.id.edit_supplier_phone_number);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu options from the res/menu/menu_editor.xml file.
+        // This adds menu items to the app bar.
+        getMenuInflater().inflate(R.menu.menu_editor, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()) {
+            // Respond to a click on the "Save" menu option
+            case R.id.action_save:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Delete" menu option
+            case R.id.action_delete:
+                // Do nothing for now
+                return true;
+            // Respond to a click on the "Up" arrow button in the app bar
+            case android.R.id.home:
+                // Navigate back to parent activity (CatalogActivity)
+                NavUtils.navigateUpFromSameTask(this);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/natasa/inventoryapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.natasa.inventoryapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Displays list of books that were entered and stored in the app
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setup FAB to open EditorActivity
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, EditorActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
