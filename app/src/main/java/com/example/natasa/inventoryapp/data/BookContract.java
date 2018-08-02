902fb6a081beac1e99010579bdad8f8c81b5fb0d
Natasa Mori <natasa@the-mori.com>
2018-08-02 22:20:40 +0200

    Added constants for book Content URI.

--- app/src/main/java/com/example/natasa/inventoryapp/data/BookContract.java
+++ app/src/main/java/com/example/natasa/inventoryapp/data/BookContract.java
@@ -1,5 +1,6 @@
 package com.example.natasa.inventoryapp.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class BookContract {
@@ -8,10 +9,28 @@ public final class BookContract {
     private BookContract() {}
 
     /**
+     * CONTENT URI
+     */
+
+    // String constant for Content Authority, the same as in the manifest
+    public static final String CONTENT_AUTHORITY = "com.example.natasa.inventoryapp";
+
+    // The base of all URI's which apps will use to contact the content provider -
+    // scheme + content authority
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    // Possible path - table name
+    public static final String PATH_BOOKS = "books";
+
+    /**
      * Inner class that defines constants for the books database table.
      * Each entry in the table represents a single book.
      */
     public static final class BookEntry implements BaseColumns {
+
+        // Full Content URI to access the book data in the provider
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_BOOKS);
+
 
         // Table name for books
         public static final  String TABLE_NAME = "books";
