679c6bd61cfc995f29f7f1d3a120fb786aab52c8
Natasa Mori <natasa@the-mori.com>
2018-08-02 22:32:03 +0200

    Added UriMatcher to ContentProvider.

--- app/src/main/java/com/example/natasa/inventoryapp/data/BookProvider.java
+++ app/src/main/java/com/example/natasa/inventoryapp/data/BookProvider.java
@@ -2,6 +2,7 @@ package com.example.natasa.inventoryapp.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 import android.support.annotation.NonNull;
@@ -11,6 +12,30 @@ public class BookProvider extends ContentProvider {
 
     /** Database helper object */
     private BookDbHelper mDbHelper;
+
+    /**
+     * URI MATCHER
+     */
+
+    // URI matcher code for the content URI for the books table
+    private static final int BOOKS = 100;
+
+    // URI matcher code for the content URI for a single book in the books table
+    private static final int BOOK_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+    // Static initializer. This is run the first time anything is called from this class.
+    static {
+        // Added 2 content URIs to URI matcher: for multiple rows and to one single row (#)
+        sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS, BOOKS);
+        sUriMatcher.addURI(BookContract.CONTENT_AUTHORITY, BookContract.PATH_BOOKS + "/#", BOOK_ID);
+    }
 
     @Override
     public boolean onCreate() {
